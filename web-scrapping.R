6a51cb013271088523c4ff79766de9f637f91d2b
Andre Campos <andreloc@gmail.com>
2019-03-14 12:52:30 +0000

    - Transformando resultado completo em data.frame - formatacao e padroes de dados

--- web-scrapping.R
+++ web-scrapping.R
@@ -11,10 +11,8 @@ library(dplyr)     ## Biblioteca auxiliar para chamada de métodos e consultas d
 library(stringr)
 library(httr)
 library(urltools)
+library(stringr)
 
-anos    <- 2019:2019                    # Anos disponíveis na página
-meses   <- str_pad(1:12, 2, pad = "0")  # Meses numéricos 01 - 12
-periodo <- expand.grid("meses" = meses, "anos" = anos)
 pdf_cache_dir <- "cache/pdf_files"
 
 # como obter esses dados? 
@@ -43,7 +41,7 @@ extrair_lista_de_parlamentares <- function(html_page) {
 }
 
 file_names <- function(ano,mes,parlamentar) {
-  parlamentar = gsub("[/. ]","_", parlamentar)
+  parlamentar = gsub("[/. ,]","_", parlamentar)
   paste0(pdf_cache_dir,"/",ano,"_",mes,"_",parlamentar,".pdf")
 }
 
@@ -57,17 +55,78 @@ download_demonstrativo_de_gastos <- function(ano, mes, parlamentar) {
   ifelse(file.exists(fileName),
          print(paste0("File already downloaded: ",fileName)),
          download.file(url,fileName))
-  lapply(fileName, pdf_text)
+  fileName
 }
 
-executar <- function(ano, mes) {
+extrair_dados_do_mes <- function(ano, mes) {
     print(paste0("Extraindo: ",ano,"-",mes))
     html          <- carregar_pagina_por_mes(ano, mes)
     parlamentares <- extrair_lista_de_parlamentares(html)
     if(length(parlamentares) == 0) {
       print("Nenhum parlamentar encontrado!")
-      return()
-    }
-    download_demonstrativo_de_gastos(ano, mes, parlamentares)
+      return(data.frame(mes = mes, 
+                  ano = ano, 
+                  parlamentar = NA,
+                  texto_pdf = NA))
+    } 
+    
+    files <- download_demonstrativo_de_gastos(ano, mes, parlamentares)
+    texto_bruto = sapply(files, pdf_text)
+    
+    # list(ano = ano, mes = mes, parlamentares = list(parlamentares), texto_bruto = texto_bruto)
+    parlamentares = expand.grid(mes = mes, 
+                ano = ano, 
+                parlamentar = parlamentares)
+    parlamentares$texto_pdf = texto_bruto
+    parlamentares
 }
+
+# 
+# Percorre os períodos definidos do ano inicial ao ano final
+# 
+# 
+criar_tabela_nao_trados <- function(ano_inicio,ano_fim) {
+  anos    <- ano_inicio:ano_fim           # Anos disponíveis na página
+  meses   <- str_pad(1:12, 2, pad = "0")  # Meses numéricos 01 - 12
+  periodos <- expand.grid("mes" = meses, "ano" = anos) # cria uma tabela com todos anos e meses
+  
+  tabela_completa <- data.frame(
+                       mes = character(),
+                       ano = character(), 
+                       parlamentar = character(), 
+                       texto_pdf = character()) 
+  
+  for( i in 1:nrow(periodos)) {
+    tabela_mes      <- extrair_dados_do_mes(periodos[i, "ano"], periodos[i, "mes"])
+    tabela_completa <- rbind(tabela_completa, tabela_mes)
+  }
+  tabela_completa
+}
+
+resultado <- criar_tabela_nao_trados(2015,2019)
+
+# dados_brutos <- mapply(executar, periodo$anos, periodo$meses)
+# dados_brutos2 <- mapply(executar, periodo$anos, periodo$meses)
+
+# dados <- data.frame(
+#   ano = dados_brutos2[1,],
+#   mes = dados_brutos2[2,]
+#   
+# )
+# 
+# extrair_dados_mes <- function(ano,mes,text_bruno) {
+#   print(ano)  
+#   
+# }
+
+# converter_texto_em_tabela <- function(texto_bruno) {
+#   
+# }
+# 
+# executar(2017,12)
+
+# mapply(extrair_dados_mes, dados_brutos)
+# View(dados_brutos[1,1])
+
+
 
