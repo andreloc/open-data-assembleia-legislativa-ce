6671b63daf0140648c10af085f9c1e1654101473
Andre Campos <andreloc@gmail.com>
2019-03-22 14:09:03 -0300

    Correção nos downloads de PDF para windows
    Simplificação para utilizar FOR no lugar de apply, visando simplificar o workshop

--- src/web-scrapping.R
+++ src/web-scrapping.R
@@ -7,7 +7,6 @@
 ## Suporte a leitura de pdfs e conversão para texto
 library(pdftools)  ## Necessária a instalação de ferramental no sistema operacional
 library(rvest)     ## Biblioteca de webscraping. Simplifica a manipulação de xml e html 
-library(dplyr)     ## Biblioteca auxiliar para chamada de métodos e consultas de forma mais semantica
 library(stringr)
 library(httr)
 library(urltools)
@@ -45,7 +44,7 @@ extrair_lista_de_parlamentares <- function(html_page) {
 # 
 file_names <- function(ano, mes, parlamentar) {
   parlamentar = gsub("[/. ,]","_", parlamentar)
-  paste0(pdf_cache_dir,"/",ano,"_",mes,"_",parlamentar,".pdf")
+  paste0(getwd(),"/",pdf_cache_dir,"/",ano,"_",mes,"_",parlamentar,".pdf")
 }
 
 #
@@ -58,9 +57,16 @@ download_demonstrativo_de_gastos <- function(ano, mes, parlamentar) {
     dir.create(pdf_cache_dir,showWarnings = T)
   }
   fileName = file_names(ano, mes, parlamentar)
-  ifelse(file.exists(fileName),
-         print(paste0("File already downloaded: ",fileName)),
-         download.file(url,fileName))
+  for (i in seq_along(url)) {
+    if(file.exists(fileName[i])) {
+      print(paste0("File already downloaded: ",fileName[i]))
+    } else {
+      download.file(url[i], fileName[i], mode = "wb")
+    }
+  }
+  # ifelse(file.exists(fileName),
+  #        ),
+  #        )
   fileName
 }
 
@@ -68,19 +74,19 @@ download_demonstrativo_de_gastos <- function(ano, mes, parlamentar) {
 # Extrai os dados de um determinado mes e cria um dataframe com 
 # 
 extrair_dados_do_mes <- function(ano, mes) {
-    print(paste0("Extraindo: ",ano,"-",mes))
-    html          <- carregar_pagina_por_mes(ano, mes)
-    parlamentares <- extrair_lista_de_parlamentares(html)
-    if(length(parlamentares) == 0) {
-      print("Nenhum parlamentar encontrado!")
-      return(data.frame(ano = ano, mes = mes, parlamentar = NA, texto_pdf = NA))
-    } 
-    
-    files <- download_demonstrativo_de_gastos(ano, mes, parlamentares)
-    texto_pdf     = sapply(files, pdf_text, USE.NAMES = F)
-    data.frame(ano = ano, mes = mes, 
-                parlamentar = parlamentares,
-                texto_pdf = texto_pdf)
+  print(paste0("Extraindo: ",ano,"-",mes))
+  html          <- carregar_pagina_por_mes(ano, mes)
+  parlamentares <- extrair_lista_de_parlamentares(html)
+  if(length(parlamentares) == 0) {
+    print("Nenhum parlamentar encontrado!")
+    return(data.frame(ano = ano, mes = mes, parlamentar = NA, texto_pdf = NA))
+  } 
+  
+  files <- download_demonstrativo_de_gastos(ano, mes, parlamentares)
+  texto_pdf     = sapply(files, pdf_text, USE.NAMES = F)
+  data.frame(ano = ano, mes = mes, 
+             parlamentar = parlamentares,
+             texto_pdf = texto_pdf)
 }
 
 # 
@@ -93,11 +99,11 @@ extrair_tabela_completa <- function(ano_inicio, ano_fim) {
   periodos <- expand.grid("mes" = meses, "ano" = anos) # cria uma tabela com todos anos e meses
   
   tabela_completa <- data.frame(
-                       ano = character(), 
-                       mes = character(),
-                       parlamentar = character(), 
-                       texto_pdf = character()
-                    ) 
+    ano = character(), 
+    mes = character(),
+    parlamentar = character(), 
+    texto_pdf = character()
+  ) 
   
   for( i in 1:nrow(periodos)) {
     tabela_mes      <- extrair_dados_do_mes(periodos[i, "ano"], 
@@ -107,7 +113,4 @@ extrair_tabela_completa <- function(ano_inicio, ano_fim) {
   tabela_completa
 }
 
-# resultado = extrair_tabela_completa(2015, 2019)
-# 
-# save(resultado,file="data.Rda")
 
