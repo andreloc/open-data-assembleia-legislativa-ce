26568408af4ea7364af5c8bcb9858e5ca9930c8e
Andre Campos <andreloc@gmail.com>
2019-03-22 13:47:51 -0300

    extração final dos dados e criação do dataframe(ano,mes,parlamentar,gasto,valor)
    Suporte à limpeza dos dados do PDF e estruturação da tabela final que contem o ano, mes, parlamentar, gasto e valor em formato de data frame.

--- run_analyses.R
+++ run_analyses.R
@@ -8,5 +8,9 @@
 source("./src/web-scrapping.R")
 source("./src/clean_data.R")
 
-resultado           <- extrair_tabela_completa(ano_inicio = 2015, ano_fim = 2019)
-resultado$texto_limp <- limpar_dados(resultado$texto_pdf)
+resultado           <- extrair_tabela_completa(ano_inicio = 2012, ano_fim = 2019)
+resultado$texto_limp <- remove_cabecalho_rodape(resultado$texto_pdf)
+
+dados_limpos <- extrair_gastos(resultado)
+
+write.csv(dados_limpos, "resultado-2012-2019.csv")
--- src/clean_data.R
+++ src/clean_data.R
@@ -1,8 +1,56 @@
-limpar_dados <- function(texto) {
+remove_cabecalho_rodape <- function(texto) {
   ## separa em linhas
-  texto = str_split(texto,"\n")[[1]]
+  texto <- sapply(texto, str_split, "\n")
+  
   ## remove 5 primeiras e 3 últimas linhas
-  texto = texto[5:(length(texto)-3)]
-  return(texto)
+  lapply(texto, function(x) {
+    if( length(x) > 7) {
+      x[5:(length(x)-3)]
+    } else {
+      ""
+    }
+  })
 }
+
+extrair_gastos_linha <- function(row) {
+  gastos <- data.frame(linha_gasto = unlist(row$texto_limp), stringsAsFactors = F)
+  char_financeiros = 20
+  
+  gastos <- gastos %>% 
+    mutate( gasto = substr(
+      linha_gasto, start = 0, 
+      stop = (nchar(linha_gasto) - char_financeiros)
+    )) %>% 
+    mutate( gasto = trimws(gasto) ) %>% 
+    mutate( valor = substr( 
+      linha_gasto, 
+      start = (nchar(linha_gasto) - char_financeiros), 
+      stop = nchar(linha_gasto))
+    ) %>% 
+    mutate( valor = str_replace(valor, "\\.","") ) %>% 
+    mutate( valor = str_replace(valor, ",",".")) %>% 
+    mutate( valor = as.numeric(trimws(valor))) %>% 
+    select(-c("linha_gasto"))
+  
+  row <- row %>% select(-c("texto_limp","texto_pdf"))
+  merge(row, gastos)
+}
+
+
+extrair_gastos <- function(tabela) {
+  dado_tratado <- data.frame(
+    ano = numeric(),
+    mes = character(),
+    parlamentar = character(),
+    gasto = character(),
+    valor = numeric()
+  )
+  for( i in 1:nrow(tabela)) {
+    dado_tratado <- rbind(dado_tratado, extrair_gastos_linha(resultado[i,]))
+  }
+  dado_tratado
+}
+
+
+
 
